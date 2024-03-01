f59eddce9fb98fa1fae66a8dd20dee2cdb02169b
Adan Sandez Zavala <adan.sandez21@gmail.com>
2024-02-29 22:05:46 -0600

    using types

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,12 @@
-export {};
+
+export function FetchAsync<T extends unknown>({query}: {query: (params?: {}) => T}): {
+    executeAsync: Awaited<T>, 
+    isLoading: boolean, 
+    error: string
+}
+
+export function FetchNow<T extends unknown>({}, query: ()=>T): {
+    data: Awaited<T>,
+    isLoading: boolean,
+    error: string
+}
